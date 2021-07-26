56b2c1f93b53c49b03d9a9eb35f493bcf2d2ac78
SlotH <jan.weiss5@gmail.com>
2021-07-26 07:08:41 +0200

    complete initial lint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "project": "tsconfig.json",
+        "project": "./tsconfig.json",
         "sourceType": "module",
         "tsconfigRootDir": __dirname
     },
