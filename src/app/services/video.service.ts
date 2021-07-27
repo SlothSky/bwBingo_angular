bdd5322d0457a9c160cad7755f456c8e9f42f817
Sloth <implimail@gmail.com>
2021-07-27 18:24:17 +0200

    introduce video service

---
+++ src/app/services/video.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class BuzzWordService
+{
+
+    apiUrl = 'http://localhost:3000';
+    buzzWordUrl = `${this.apiUrl}/api/buzzWord`;
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    
+
+}
